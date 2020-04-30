86f844319d25bfbbbe7beb2f7f6035e446b658e8
Rey Aparece <ApareceR12@gmail.com>
2020-04-29 19:35:16 -0500

    Add project files.

---
+++ TPPhotography/bgcenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Apare\OneDrive\Documents\MyWebSites\PersonalWebsites\TPPhotography\bgcenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ TPPhotography/bgcenv/myproject/myproject/views.py
@@ -0,0 +1,18 @@
+﻿from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create views
+
+def home(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    return render(request, "home.html",{})
+
+def contact(request, *args, **kwargs):
+    return render(request, "contact.html",{})
+
+def about(request, *args, **kwargs):
+    return render(request, "about.html",{})
+
+def portfolio(request, *args, **kwargs):
+    return render(request, "portfolio.html",{})
